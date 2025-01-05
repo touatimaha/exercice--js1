7128fff2a89035ec1694ee019cabde0a62311f29
touatimaha <mahatouati954@gmail.com>
2025-01-05 19:44:39 +0100

    exercice

---
+++ app.js
@@ -0,0 +1,26 @@
+let marksMass="78 kg" ;
+console.log(typeof marksMass )
+let marksHeight="1.96 m" ;
+console.log(typeof marksHeight)
+
+
+let johnMass ="92 kg" ;
+console.log(typeof johnMass)
+let johnHeight="1.95 m" ;
+console.log(typeof johnHeight)
+
+
+// let BMI = Mass / Height * 2 ;
+// BMI = 78 / 1.96 *2 ;
+// console.log(BMI)
+
+
+const now = 78
+const BMI = now * 1.96 / 2 ; 
+console.log(BMI)
+
+
+// let marksMass = 78 ; 
+// let johnMass = 92 ;
+// let BMI = marksMass > johnMass
+// console.log(typeof BMI) ; 
